f388f6d9cc6f87f3dc8ba9730f3edc7ebc320859
Matthew Cerha <m.cerha@fetchrewards.com>
2026-07-07 17:58:17 -0500

    prompt change, add gcloud cli tools to workbit

--- zsh/.config/zsh/hosts/workbit/interactive.zsh
+++ zsh/.config/zsh/hosts/workbit/interactive.zsh
@@ -1,3 +1,5 @@
 # Machine-specific interactive settings for workbit.
 
 fpath=($HOME/.docker/completions $fpath)
+
+source /opt/homebrew/share/google-cloud-sdk/completion.zsh.inc
--- zsh/.config/zsh/hosts/workbit/paths.zsh
+++ zsh/.config/zsh/hosts/workbit/paths.zsh
@@ -1,1 +1,3 @@
 add_to_path "/Applications/SnowSQL.app/Contents/MacOS"
+
+source /opt/homebrew/share/google-cloud-sdk/path.zsh.inc
